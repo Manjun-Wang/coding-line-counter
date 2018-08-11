945e0c74bf02df0b6775162d8aa605163756aedc
Manjun <42054560+Manjun-Wang@users.noreply.github.com>
2018-08-10 23:48:31 -0400

    Update app.py

--- com/path/app.py
+++ com/path/app.py
@@ -13,8 +13,11 @@ def main(argv=None):
         argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+            opts, args = getopt.getopt(argv[1:], "h", ["help", "file=", "language="])
+
             print(opts, args)
+            options = get_correct_opts(opts)
+            
         except getopt.error as msg:
             raise Usage(msg)
         # more code, unchanged
@@ -24,6 +27,17 @@ def main(argv=None):
         return 2
 
 
+def get_correct_opts(opts):
+    options = {}
+    for opt in opts:
+        if opt[0] == "--file":
+            options["file"] = opt[1]
+        if opt[0] == "--language":
+            options["--language"] = opt[1]
+    if len(options) != 2:
+        raise Usage("Need to have --file=x and --language=y as input params")
+    return options
+
+
 if __name__ == "__main__":
-    print(1)
     sys.exit(main())
